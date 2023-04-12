58fa2b523455db83ac5e1281ba824155e4565f00
vijaymanjunathas5 <vijaymanjunathas5@gmail.com>
2023-04-12 11:10:19 +0530

    same line merger try

--- Deploy.sh
+++ Deploy.sh
@@ -6,3 +6,4 @@ addding this line for testing merge test one more time from both brandh
 adding this line from Devvelopment for testing merge on both brahch
  development
  merge edited for both branch entries
+ testigng whether same line edit causing merger conflict
