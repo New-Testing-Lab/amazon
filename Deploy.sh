6e33244cbfe9e0b2823ce9430c5638d3395bb569
vijaymanjunathas5 <vijaymanjunathas5@gmail.com>
2023-04-24 12:07:28 +0530

    checking merge for branch

--- Deploy.sh
+++ Deploy.sh
@@ -2,6 +2,7 @@ This file contains the Status related info.
 adding line here to test merte same ine conflict from master
 adding second line after git commit
 updated this line in Development branch for merge 
+checking commint from one branch to all or only the branch which we applied merge
 addint anothe4r line or merge test
 addding this line for testing merge test one more time from both brandh
 adding this line from Devvelopment for testing merge on both brahch
